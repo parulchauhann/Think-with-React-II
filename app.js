0edefdd8e11e99a4ff988788e91af8d4e150b489
Parul <parul1374.be23@chitkarauniversity.edu.in>
2024-02-03 09:24:16 +0530

    think with react 2

---
+++ app.js
@@ -0,0 +1,13 @@
+class ReactContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello! Welcome to Kalvium!
+                <div>This is babel</div>
+            </div>
+        )
+    }
+}
+
+const container = document.getElementById('react-container')
+ReactDOM.render(<ReactContainer/>, container)
